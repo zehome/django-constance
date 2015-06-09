e4af29f0c6a7d10a832467f9bb1ffbf55f3f6ffe
Lee Solway <lee@jp74.com>
2015-06-09 23:08:07 +0100

    removed name for Django 1.8 - added version switch

--- constance/models.py
+++ constance/models.py
@@ -6,19 +6,24 @@ def create_perm(app, created_models, verbosity, db, **kwargs):
     Creates a fake content type and permission
     to be able to check for permissions
     """
+    from django import VERSION
     from django.contrib.auth.models import Permission
     from django.contrib.contenttypes.models import ContentType
 
-    if ContentType._meta.installed and Permission._meta.installed:
+    if VERSION >= (1, 8):
+        content_type, created = ContentType.objects.get_or_create(
+            app_label='constance',
+            model='config')
+    else:
         content_type, created = ContentType.objects.get_or_create(
             name='config',
             app_label='constance',
             model='config')
 
-        permission, created = Permission.objects.get_or_create(
-            name='Can change config',
-            content_type=content_type,
-            codename='change_config')
+    permission, created = Permission.objects.get_or_create(
+        name='Can change config',
+        content_type=content_type,
+        codename='change_config')
 
 
 signals.post_syncdb.connect(create_perm, dispatch_uid="constance.create_perm")
